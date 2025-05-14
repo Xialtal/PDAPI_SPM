14631b64621233f57e1771342835a7775850a0d9
Ilia Lubianoi <subvert.dev@gmail.com>
2025-05-14 20:44:10 +0300

    Update

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PDAPI",
-            url: "https://github.com/SubvertDev/PDAPI_SPM/releases/download/0.4.1/PDAPI.zip",
-            checksum: "266a6622af8fc116598e49920923862d385bb303e336edc342e73e572b303eab"
+            url: "https://github.com/SubvertDev/PDAPI_SPM/releases/download/0.4.2/PDAPI.zip",
+            checksum: "e9b83ad60d704731a630d38844ffbb60c7450a31596e2097ff382a212a847435"
         )
     ]
 )
